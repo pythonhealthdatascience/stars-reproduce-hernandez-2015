2b3a262ed61a2f8f30604686869e5369561ab287
amyheather <a.heather2@exeter.ac.uk>
2024-10-04 10:06:16 +0100

    refactor(reproduction): turned Experiment1.py into main.py, with ability to then call to it from new Experiment1.py, to reproduce code duplication for new experiments

--- reproduction/python_scripts/Experiment1.py
+++ reproduction/python_scripts/Experiment1.py
@@ -3,70 +3,23 @@
 # Alter percentage of pre-screened (10%, 20%, 30%,... 90%)
 # Used to produce Figure 5 and 6
 
-# Import required libraries 
-import datetime
 from os import listdir
 from os.path import isfile, join
-import os, sys
-import ParameterReader
-import SolutionWriter
-import ExperimentRunner
-from multiprocessing import Pool
+from main import run_experiment
 
 # Path to scenarios
 mypath = r'../inputs'
+
+# Name of experiment files identified from path to scenarios
 experimentFiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 # Path to save results
 experimentFolder = '../python_outputs/experiment1'
 
-# Seeds for reproducibility
-seeds = [123, 456, 789]
-
-
-def run_scenario(file):
-    '''
-    Run algorithm using the pre-screened percentage from the provided file.
-
-    Parameters:
-    ----------
-    file : string
-      Path to text file containing pre-screened percentage
-    '''
-    # Run the analysis
-    parameterReader = ParameterReader.ParameterReader(join(mypath,
-                                                           file))
-    experimentRunner = ExperimentRunner.ExperimentRunner(seeds,
-                                                         parameterReader)
-    experimentRunner.run(runs=1,
-                         population=100,
-                         generations=1)
-
-    # Get the pre-screened percentage for that scenario and folder
-    scenario = int(parameterReader.parameters['preScreenedPercentage']*100)
-    scenarioFolder = experimentFolder + '/prescreen' + str(scenario)
-
-    # Save the results
-    solutionWriter = SolutionWriter.SolutionWriter(
-        join(mypath, file), experimentRunner,
-        folderName=scenarioFolder)
-    solutionWriter.dumpSolution()
-
-
-# Start timer
-startTime = datetime.datetime.now()
-
-# Create a process pool that uses all CPUs
-pool = Pool()
-try:
-    # Map the run_scenario function to the experiment files
-    results = pool.map(run_scenario, experimentFiles)
-finally:
-    # Close the pool and wait for the worker processes to finish
-    pool.close()
-    pool.join()
-
-# End timer and save time for that result
-endTime = datetime.datetime.now()
-with open(os.path.join(experimentFolder, "time.txt"), "w") as text_file:
-    text_file.write("{0}".format(endTime - startTime))
+# Run expertiment
+run_experiment(mypath=mypath,
+               experimentFolder=experimentFolder,
+               experimentFiles=experimentFiles,
+               runs=1,
+               population=1,
+               generations=1)
---
+++ reproduction/python_scripts/main.py
@@ -0,0 +1,117 @@
+# Main script to run model for specified scenarios
+
+import datetime
+import os
+from os.path import join
+import ParameterReader
+import SolutionWriter
+import ExperimentRunner
+from multiprocessing import Pool
+
+
+def run_scenario(mypath, experimentFolder, file, runs, population,
+                 generations):
+    '''
+    Run algorithm using the pre-screened percentage from the provided file.
+
+    Parameters:
+    ----------
+    mypath : string
+        Path to scenarios
+    experimentFolder : string
+        Path to save results
+    file : string
+        Path to text file containing pre-screened percentage
+    runs : integer
+        Number of times to repeat analysis
+    population: integer
+        Size of population
+    generations : integer
+        Number of generations
+    '''
+    # Seeds for reproducibility
+    seeds = [123, 456, 789]
+
+    # Run the analysis
+    parameterReader = ParameterReader.ParameterReader(join(mypath,
+                                                           file))
+    experimentRunner = ExperimentRunner.ExperimentRunner(seeds,
+                                                         parameterReader)
+    experimentRunner.run(runs=runs,
+                         population=population,
+                         generations=generations)
+
+    # Get the pre-screened percentage for that scenario and folder
+    scenario = int(parameterReader.parameters['preScreenedPercentage']*100)
+    scenarioFolder = experimentFolder + '/prescreen' + str(scenario)
+
+    # Save the results
+    solutionWriter = SolutionWriter.SolutionWriter(
+        os.path.join(mypath, file), experimentRunner,
+        folderName=scenarioFolder)
+    solutionWriter.dumpSolution()
+
+
+def wrapper(d):
+    '''
+    Wrapper function to allow input of dictionary tp Pool() for run_scenario()
+
+    Parameters:
+    -----------
+    d : dictionary
+        Dictionary containing parameters to input to run_scenario()
+    '''
+    return run_scenario(**d)
+
+
+def run_experiment(mypath, experimentFolder, experimentFiles, runs, population,
+                   generations):
+    '''
+    Run set of scenarios using parallel processing, and record time elapsed
+
+    Parameters:
+    -----------
+    mypath : string
+        Path to scenarios
+    experimentFolder : string
+        Path to save results
+    experimentFiles : list
+        List where each element is a string path to a text file containing a
+        pre-screened percentage
+    runs : integer
+        Number of times to repeat analysis
+    population: integer
+        Size of population, representing number of individuals in each
+        generation
+    generations : integer
+        Number of generations to run algorithm for, which determines how long
+        algorithm will evolve for solutions
+    '''
+    # Start timer
+    startTime = datetime.datetime.now()
+
+    # Create parameter list for each experiment
+    params = [
+        {'mypath': mypath,
+         'experimentFolder': experimentFolder,
+         'file': file,
+         'runs': runs,
+         'population': population,
+         'generations': generations}
+        for file in experimentFiles
+    ]
+
+    # Create a process pool that uses all CPUs
+    pool = Pool()
+    try:
+        # Map the run_scenario function to the experiment files
+        results = pool.map(wrapper, params)
+    finally:
+        # Close the pool and wait for the worker processes to finish
+        pool.close()
+        pool.join()
+
+    # End timer and save time for that result
+    endTime = datetime.datetime.now()
+    with open(join(experimentFolder, "time.txt"), "w") as text_file:
+        text_file.write("{0}".format(endTime - startTime))
