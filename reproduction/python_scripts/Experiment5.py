ff9fc47d5d9f30f7fa6a049191b678271070bf39
amyheather <a.heather2@exeter.ac.uk>
2024-10-07 12:08:03 +0100

    fix(reproduction): corrected experiment5

--- reproduction/python_scripts/Experiment5.py
+++ reproduction/python_scripts/Experiment5.py
@@ -6,34 +6,61 @@
 # (Intel Core i9-13900K with 81GB RAM running Pop!_OS 22.04 Linux)
 
 from os.path import join
+import datetime
 import sys
 sys.path.append('./myutils')
 import myutils
-import ParameterReader
-import SimulatorRunner
-import StaffAllocationProblem
+import PODSimulation
+import ResultsAnalyzer
 
-# Set capacities
+# Path to save results
+experimentFolder = '../python_outputs/experiment5'
+
+# Code adapted from PODSimulation.py...
+
+def get_20_seeds():
+    seeds = [2308947, 
+             982301, 
+             329, 
+             12389, 
+             34324,
+             45645,
+             45456546,
+             681683,
+             7,
+             543,
+             3982473289,
+             1321,
+             798789,
+             8809,
+             35797,
+             43,
+             879,
+             32432,
+             78987,
+             675489]
+    return seeds
+
+# Start timer
+startTime = datetime.datetime.now()
+
 capacities = [4, 6, 6, 1]
 
-# Seeds as in main.py
-seeds = [123, 456, 789]
+seeds = get_20_seeds()
 
-# Get pre-screening parameter as in main.py
-mypath = r'../inputs'
-file = '10-prescreened.txt'
-parameterReader = ParameterReader.ParameterReader(join(mypath, file))
+simulations = []
+for seed in seeds:
+    # Run simulation
+    simul = PODSimulation.PODSimulation(capacities)
+    simul.model(seed)
+    # Save throughput from each run to a list
+    simulations.append(simul.get_number_out())
 
-# Code to run simulation as from StaffAllocationProblem.py
-simulatorRunner = SimulatorRunner.SimulatorRunner()
-simulatorRunner.run(seeds=seeds,
-                    capacities=capacities,
-                    parameterReader=parameterReader)
-time = simulatorRunner.get_avg_waiting_times()
-resources = simulatorRunner.get_resource_count()
-throughput = simulatorRunner.get_processed_count()
+# Save results to file
+with open(join(experimentFolder, "results.txt"), "w") as text_file:
+    text_file.write(str(simulations))
 
-# Print results
-print(time)
-print(resources)
-print(throughput)
+# End timer and save time for that result
+endTime = datetime.datetime.now()
+with open(join(experimentFolder, "time.txt"), "w") as text_file:
+    text_file.write("{0}".format(endTime - startTime))
